ecdde647a5224f0f02168b4eed47fb01a13f006f
IncredibleSnake <72911332+IncredibleSnake@users.noreply.github.com>
2021-05-22 12:58:02 +0300

    Fixed some things

--- CalculatorShkut/CalculatorShkut/Form1.Designer.cs
+++ CalculatorShkut/CalculatorShkut/Form1.Designer.cs
@@ -42,20 +42,20 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(57, 33);
+            this.label1.Location = new System.Drawing.Point(57, 42);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(114, 13);
+            this.label1.Size = new System.Drawing.Size(112, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Ведите первое число";
+            this.label1.Text = "Введіть перше число";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(265, 33);
+            this.label2.Location = new System.Drawing.Point(265, 42);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(119, 13);
+            this.label2.Size = new System.Drawing.Size(108, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Ведите втоорое число";
+            this.label2.Text = "Введіть друге число";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // textBox1
@@ -75,58 +75,59 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(57, 116);
+            this.label3.Location = new System.Drawing.Point(57, 81);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(110, 13);
+            this.label3.Size = new System.Drawing.Size(100, 13);
             this.label3.TabIndex = 4;
-            this.label3.Text = "Выбирете операцию";
+            this.label3.Text = "Виберіть операцію";
             // 
             // comboBox1
             // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
             "+",
             "-",
             "*",
             "/"});
-            this.comboBox1.Location = new System.Drawing.Point(60, 146);
+            this.comboBox1.Location = new System.Drawing.Point(60, 97);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.Size = new System.Drawing.Size(121, 32);
             this.comboBox1.TabIndex = 5;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(60, 190);
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Location = new System.Drawing.Point(60, 135);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 6;
-            this.button1.Text = "Посчитать ";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Text = "Порахувати";
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(268, 190);
+            this.button2.Location = new System.Drawing.Point(268, 135);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 7;
-            this.button2.Text = "Очистить";
+            this.button2.Text = "Очистити";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(60, 253);
+            this.textBox3.Location = new System.Drawing.Point(60, 164);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(336, 20);
             this.textBox3.TabIndex = 8;
-            this.textBox3.Text = "я";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(459, 337);
+            this.ClientSize = new System.Drawing.Size(459, 217);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -137,7 +138,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Калькулятор";
             this.ResumeLayout(false);
             this.PerformLayout();
 

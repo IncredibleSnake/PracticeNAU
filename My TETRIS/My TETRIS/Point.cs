d89c1ca5837595633e1b1697315afff87b606311
IncredibleSnake <72911332+IncredibleSnake@users.noreply.github.com>
2021-06-05 12:01:03 +0300

    TETRIS Console Start

---
+++ My TETRIS/My TETRIS/Point.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_TETRIS
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char c;
+
+        public Point(int a, int b, char sym)
+        {
+            x = a;
+            y = b;
+            c = sym;
+        }
+        public Point() { }
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(c);
+        }
+    }
+}
---
+++ My TETRIS/My TETRIS/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_TETRIS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(40, 30);
+            Console.SetBufferSize(40, 30);
+
+
+            Point p1 = new Point(2, 3, '*');
+            p1.Draw();
+
+            Square s1 = new Square(5, 6, '*');
+            s1.Draw();
+
+            Stick st1 = new Stick(10, 3, '$');
+            st1.Draw();
+
+            Console.ReadLine();
+        }
+
+    }
+}
